7b5d406fdf0c385abcf2c03822daeecb37ccf9b3
Firdaus Adi Putra <mail@firdaus.org>
2025-12-24 08:30:36 +0700

    Fix links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://appleinautumn.github.io",
-  base: "/research-medical-llm/",
+  base: "/research-medical-llm-html/",
 });
